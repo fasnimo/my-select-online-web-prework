5f083933ffcc9c085a0ad2206b65802ac6ae97b6
Rene Cocom <fasnimo@gmail.com>
2019-04-26 03:10:57 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ collection = [1, 2, 3, 4, 5]
 
 def my_select(collection)
   collect = []
-  (1, 2, 3, 4, 5).select do |num|
+  (1, 2, 3, 4, 5)to_a.select do |num|
    num.even? 
    end 
  collection
