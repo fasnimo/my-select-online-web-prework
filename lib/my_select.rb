7d68cf5be619bd3d1f0059bb4280e4266ebcdfd8
Rene Cocom <fasnimo@gmail.com>
2019-04-26 03:10:51 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,21 +2,9 @@ collection = [1, 2, 3, 4, 5]
 
 def my_select(collection)
   collect = []
-  [1, 2, 3, 4, 5].select do |num|
+  (1, 2, 3, 4, 5).select do |num|
    num.even? 
    end 
  collection
 end 
 
-# [1,2,3,4,5,6].sort do |x,y|
-#   if x.odd? and y.odd?
-#     0
-#   elsif x.odd?
-#     -1
-#   else
-#     1
-# end  
-#   if (x.odd? && y.odd?) or (x.even? && y.even?)
-#   x <=> y
-# end 
-# end
